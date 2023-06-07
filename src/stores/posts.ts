d3e57788aaa2d2ca321f1b29b756ee231893d538
‘2458578637’ <2458578637@qq.com>
2023-06-08 00:11:31 +0800

    1

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -4,3 +4,5 @@ export const allPostList = atom() as any
 export const pageProp = atom() as any
 export const postProp = atom() as any
 export const metaProp = atom() as any
+export const categorieProp = atom() as any
+export const categories = atom() as any
